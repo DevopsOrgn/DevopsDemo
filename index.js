6f76882eb96f71de9ef7010e73f0b34d8c0521e8
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2021-02-12 15:42:25 +0530

    re deploy app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req, res)=> res.send('second commit runner to deployn.....'));
+app.get('/', (req, res)=> res.send('re deploy'));
 
 const portn = process.env.port || 8010;
 app.listen(portn, ()=>{
