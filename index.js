2c2e1507d2a9a824a93eb39d84e621e9b1ebed40
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2021-02-12 15:10:19 +0530

    test2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res)=> res.send('Simple test update checkin after runner listen.....'));
 
-const portn = process.env.port || 3000;
+const portn = process.env.port || 8010;
 app.listen(portn, ()=>{
     console.log('my api is running on '+ portn);
 })
